99175d803d69be6ab7aa4c70e9cae7379a275e4f
Thiago Simoes <thiago.simoes@ufrj.br>
2025-08-16 10:12:26 -0300

    fix: solved pool initialization

--- src/dbconnection.jl
+++ src/dbconnection.jl
@@ -6,7 +6,14 @@ using .Pool
 # ---------------------------
 # Connection to the database
 # ---------------------------
+__POOL_INITIALIZED__ = false
+
 function dbConnection()
+    if !__POOL_INITIALIZED__
+        Pool.initPool()
+        global __POOL_INITIALIZED__ = true
+    end
+    
     return Pool.getConnection()
 end
 
